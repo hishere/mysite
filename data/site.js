3765dd59238ca56c652e415f5ee5bafc6493aeb1
Nooneb <1971902975@qq.com>
2022-02-20 18:03:42 +0800

    同步gitee

---
+++ data/site.js
@@ -0,0 +1,231 @@
+function sites() {
+    return [
+        {
+            title: "搜索引擎",
+            sites: [
+                {text: "手机百度", url: "http://m.baidu.com"},
+                {text: "电脑百度", url: "http://www.baidu.com"},
+                {text: "神马搜索", url: "http://so.m.sm.cn"},
+                {text: "搜狗", url: "http://m.sogou.com"},
+                {text: "必应Bing", url: "https://cn.bing.com"},
+                {text: "Google", url: "https://www.google.com"},
+            ]
+        },
+        {
+            title: "影视",
+            sites: [
+                {text: "iMyShare视频解析", url: "https://imyshare.com/free-movie"},
+                {text: "落尘之木视频解析", url: "https://video.luochenzhimu.com"},
+
+                {text: "点亮网页版", url: "http://zhuye-1256787622.cos.ap-beijing.myqcloud.com/dianliangyingyong.html"},
+                {text: "不可思议导航", url: "https://orxing.top/nav"},
+                {text: "奈菲影视", url: "https://www.nfmovies.com"},
+                {text: "低端影视", url: "https://ddrk.me"},
+                {text: "奇米奇米", url: "http://www.qimiqimi.com"},
+                {text: "夏沫电影", url: "https://www.xmdyw.net"},
+                {text: "vip部落", url: "http://www.vipbuluo.com"},
+                {text: "阳光电影", url: "https://www.ygdy8.com"},
+
+                {text: "星火发布页", url: "https://docs.qq.com/sheet/DSndLS0xUaG9WS1Fm?tab=BB08J2"},
+                {text: "视频解析", url: "https://tv.hwkxk.cn"},
+                {text: "真不卡", url: "http://www.zhenbuka3.com"},
+                {text: "大师兄", url: "https://dsxys.com"},
+                {text: "刘明野的vip视频解析", url: "http://tool.liumingye.cn/video"},
+            ]
+        },
+        {
+            title: "音乐",
+            sites: [
+                {text: "悦音App(wx:小轻音)", url: "https://wwr.lanzoux.com/b02ie3p0d"},
+                {text: "在线音乐", url: "https://music.postudy.cn"},
+                {text: "音乐助手", url: "https://music.hwkxk.cn"},
+                {text: "静听音乐App", url: "https://xiaozao.lanzoui.com/b04aslz3c"},
+                {text: "听●下App", url: "https://dmla.lanzoui.com/b05m79b3i"},
+                {text: "Listen 1", url: "http://listen1.github.io/listen1"},
+                {text: "HiFiNi", url: "https://www.hifini.com"},
+            ]
+        },
+        {
+            title: "社区",
+            sites: [
+                {text: "百度贴吧", url: "https://tieba.baidu.com"},
+                {text: "新浪微博", url: "https://m.weibo.cn"},
+                {text: "Telegram", url: "https://web.telegram.org"},
+                {text: "Twitter", url: "https://mobile.twitter.com"},
+                {text: "FaceBook", url: "https://m.facebook.com"},
+                {text: "出国推荐", url: "https://m.egouz.com"},
+                {text: "YouTobe", url: "https://m.youtube.com"},
+                {text: "书签地球", url: "https://www.bookmarkearth.com"},
+                {text: "资源汇", url: "http://ziyuanhuishequ.ysepan.com"},
+                {text: "哆啦A梦的口袋", url: "http://m.ysepan.com/baozangku"},
+                {text: "雨过天晴", url: "http://ygtq.ysepan.com"},
+                {text: "GG0", url: "https://gg0.net"},
+            ]
+        },
+        {
+            title: "学习",
+            sites: [
+                {text: "哔哩哔哩", url: "https://m.bilibili.com"},
+                {text: "慕课中国", url: "https://www.mooc.cn"},
+                {text: "慕课网", url: "https://m.imooc.com"},
+                {text: "虫虫教学", url: "https://bestcclm.com/portal.php"},
+                {text: "iBooker", url: "https://home.apachecn.org"},
+            ]
+        },
+        {
+            title: "PC软件",
+            sites: [
+                {text: "老殁", url: "https://www.mpyit.com"},
+                {text: "果核剥壳", url: "https://www.ghxi.com"},
+                {text: "非凡软件站", url: "https://www.crsky.com"},
+                {text: "心海e站", url: "https://www.hrtsea.com"},
+                {text: "六音软件", url: "https://www.6yit.com"},
+                {text: "Win10溯潮汐", url: "https://www.winos.me"},
+                {text: "MSDN I tell you", url: "http://msdn.itellyou.cn"},
+                {text: "NEXT I tell you", url: "https://next.itellyou.cn"},
+                {text: "423 Down", url: "https://www.423down.com"},
+                {text: "异星", url: "https://www.yxssp.com"},
+                {text: "无痕哥", url: "https://www.whg6.com"},
+                {text: "易酷软件", url: "https://www.yikurj.com"},
+                {text: "亿破姐", url: "https://www.ypojie.com"},
+                {text: "落尘之木", url: "https://www.luochenzhimu.com"},
+                {text: "木风软件站", url: "https://www.mfrjz.com"},
+                {text: "独孤求软", url: "http://www.dugubest.com"},
+                {text: "乐软博客", url: "https://www.isharepc.com"},
+                {text: "chrome插件下载", url: "https://www.extfans.com"},
+                {text: "效率工具utools", url: "https://u.tools"},
+                {text: "小爱密钥共享", url: "http://webact.185.hk/sharedkey.php"},
+
+            ]
+        },
+        {
+            title: "安卓软件",
+            sites: [
+                {text: "豌豆荚", url: "https://m.wandoujia.com"},
+                {text: "神马应用", url: "https://m.sm.cn"},
+                {text: "手机乐园", url: "https://m.shouji.com.cn"},
+                {text: "PP助手", url: "https://m.pp.cn"},
+                {text: "移动叔叔", url: "http://bbs.ydss.cn"},
+                {text: "魔趣", url: "https://download.mokeedev.com"},
+                {text: "聚应用App", url: "http://alipanso6.com"},
+                {text: "yoyodadada", url: "https://lanzoux.com/u/yoyodadada"},
+                {text: "蓝奏云列表", url: "https://imyshare.com/static/others/LanZousResource.txt"},
+                {text: "蓝奏云列表", url: "https://imyshare.com/static/others/PersonalDrive.txt"},
+
+                {text: "乐愚社区", url: "https://bbs.leyuz.net/t/192371"},
+
+                {text: "好玩网", url: "https://www.playes.net"},
+                {text: "天火同人", url: "https://appstore.trgame.cn"},
+                {text: "欧陆系列", url: "https://www.ieasytech.com"},
+                {text: "畅玩空间", url: "https://www.wo1wan.com"},
+                {text: "骑士下载", url: "https://www.vqs.com"},
+                {text: "TV软件", url: "https://www.sharertv.com"},
+
+            ]
+        },
+        {
+            title: "网盘",
+            sites: [
+                {text: "阿里云盘", url: "https://www.aliyundrive.com/drive"},
+                {text: "百度网盘", url: "https://pan.baidu.com"},
+                {text: "m盘多多", url: "http://m.panduoduo.online"},
+                {text: "w盘多多", url: "http://www.panduoduo.online"},
+                {text: "KinhDown", url: "https://kinhdown.com"},
+                {text: "奈斯搜索", url: "https://www.niceso.fun"},
+                {text: "网盘资源分享", url: "https://www.yunpanziyuan.com"},
+                {text: "Telegram中文搜索", url: " http://www.sssoou.com"},
+
+
+            ]
+        },
+        {
+            title: "模拟器",
+            sites: [
+                {text: "52emu", url: "http://java.52emu.cn"},
+                {text: "爱cia", url: "https://lovecia.com"},
+                {text: "爱3ds", url: "https://i3ds.fun"},
+                {text: "蘑菇下载器steam", url: "https://lanzoux.com/i70q2fa"},
+                {text: "GBT游戏空间",  url: "http://gbtgame.ysepan.com"},
+                {text: "小霸王",  url: "https://www.yikm.net"},
+                {text: "魔方在线",  url: "https://iamthecu.be"},
+
+            ]
+        },
+        {
+            title: "知识",
+            sites: [
+                {text: "百度百科", url: "https://baike.baidu.com"},
+                {text: "MBA智库", url: "https://www.mbalib.com"},
+                {text: "文言文字典", url: "https://wyw.hwxnet.com"},
+                {text: "古诗文网", url: "https://m.gushiwen.org"},
+                {text: "51VOA", url: "https://m.51voa.com"},
+                {text: "CGTN", url: "https://www.cgtn.com"},
+                {text: "菁优网", url: "http://www.jyeoo.com"},
+                {text: "乐乐课堂", url: "http://m.leleketang.com"},
+                {text: "瑞文文摘", url: "https://www.rwtext.com"},
+            ]
+        },
+        {
+            title: "编程",
+            sites: [
+                {text: "MVN Repository", url: "https://mvnrepository.com"},
+                {text: "阿里云Maven", url: "https://developer.aliyun.com/mvn/view"},
+                {text: "W3 school", url: "https://www.w3school.com.cn"},
+                {text: "W3C school", url: "https://www.w3cschool.cn"},
+                {text: "菜鸟教程", url: "https://www.runoob.com"},
+                {text: "java1234", url: "http://www.java1234.com"},
+                {text: "java1234下载", url: "http://download.java1234.com"},
+                {text: "V5在线教程", url: "http://www.vue5.com"},
+                {text: "Web框架大全", url: "https://www.techempower.com/benchmarks"},
+                {text: "45个CSS形状", url: "https://www.polebrief.com/index"},
+                {text: "cdnjs", url: "https://cdnjs.com"},
+                {text: "WebJars", url: "https://www.webjars.org"},
+                {text: "JitPack", url: "https://jitpack.io"},
+                {text: "饿了吗UI", url: "https://element.eleme.cn"},
+                {text: "微信公众平台", url: "https://mp.weixin.qq.com"},
+            ]
+        },
+        {
+            title: "工具",
+            sites: [
+                {text: "在线传文本", url: " https://airportal.cn"},
+                {text: "文叔叔传文件", url: "https://www.wenshushu.cn"},
+                {text: "八爪鱼采集", url: "https://https://www.bazhuayu.com"},
+                {text: "透明图", url: "http://pngimg.com"},
+                {text: "爱稀奇", url: "http://www.ixiqi.com"},
+                {text: "兔二工具", url: "https://www.17he.net"},
+                {text: "Miku工具", url: "https://tools.miku.ac"},
+                {text: "孟坤工具箱", url: "http://tool.mkblog.cn"},
+                {text: "iMyShare导航", url: "https://imyshare.com"},
+                {text: "接收验证码", url: "https://jiemahao.com"},
+                {text: "接收邮箱", url: "http://24mail.chacuo.net"},
+                {text: "图标库", url: "https://iconpark.oceanengine.com/official"},
+                {text: "鼠标增强", url: "https://github.com/majorworld/MousePlus"},
+                {text: "鼠标手势", url: "https://shuax.com/project/mouseinc"},
+                {text: "HiPC远程控制", url: "https://docs.hipc.cn"},
+                {text: "极简简历", url: "https://www.polebrief.com/index"},
+                {text: "ico在线转", url: "http://www.ico51.cn"},
+                {text: "进制计算器", url: "https://calculator.yunser.com"},
+                {text: "3d max在线", url: "https://www.tooleyes.com/app/3d_editor.html"},
+                {text: "图片编辑在线", url: "https://photokit.com/editor"},
+                {text: "安卓图标工厂", url: "https://www.tooleyes.com/app/icon_studio.html"},
+            ]
+        },
+        {
+            title: "书籍",
+            sites: [
+                {text: "脚本之家电子书", url: "https://www.jb51.net/books"},
+                {text: "掌阅万本书籍", url: "https://sn9.us/dir/17192461-27868687-4e3441"},
+                {text: "lorefree", url: "https://ebook2.lorefree.com"},
+                {text: "辣豆瓣", url: "http://www.ladouban.com"},
+                {text: "EPUBB整站电子书", url: "https://www.aliyundrive.com/s/MJ7qoRVpnT3"},
+                {text: "樊登2562份", url: "https://www.aliyundrive.com/s/4dYTVHpcVcB"},
+                {text: "知乎1090篇", url: "https://www.aliyundrive.com/s/DaNNsdvi9bi"},
+                {text: "极客时间 - 专栏", url: "https://www.aliyundrive.com/s/rFnFNmJmo84"},
+                {text: "极客时间 - 视频", url: "https://www.aliyundrive.com/s/YCZqsjdZ3wC"},
+                {text: "公开课知识库", url: "https://docs.qq.com/sheet/DRU5MWHZCTHFGQnhM"},
+            ]
+        }
+    ]
+}
+
---
+++ data/test.js
@@ -0,0 +1,6 @@
+function test1(){
+    return "你好"
+}
+function test2(){
+    return "hello"
+}
